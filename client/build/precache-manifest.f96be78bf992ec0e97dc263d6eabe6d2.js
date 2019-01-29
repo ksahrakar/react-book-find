e0853f4556630e078a9ff84358d3afca45254515
Kam Sah <ksahrakar@gmail.com>
2019-01-28 21:20:43 -0800

    update express static

---
+++ client/build/precache-manifest.f96be78bf992ec0e97dc263d6eabe6d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5342bde34c488e44faac",
+    "url": "/static/js/main.5342bde3.chunk.js"
+  },
+  {
+    "revision": "7ce865f202d4292aae03",
+    "url": "/static/js/1.7ce865f2.chunk.js"
+  },
+  {
+    "revision": "5342bde34c488e44faac",
+    "url": "/static/css/main.108a87ac.chunk.css"
+  },
+  {
+    "revision": "96229bf1ce508984582424e996a75ad2",
+    "url": "/index.html"
+  }
+];
